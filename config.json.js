04a99949b27a50f07dc2a2a52d10ad6b9af6aa13
lixin <sadoneli@gmail.com>
2016-11-28 09:23:37 +0800

    first commit

---
+++ config.json.js
@@ -0,0 +1,9 @@
+{
+    "build_date": "2016-11-25_21:35:44", 
+    "description": "来自网络的胃軟系统工具", 
+    "home_url": "Module_kms.asp", 
+    "md5": "48dda9009eeb736188ba8f40e127d673", 
+    "module": "kms", 
+    "title": "系统工具", 
+    "version": "1.0"
+}
